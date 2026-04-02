6baa0dd4fc9f2062be3a50c0452210a70e7f0147
Dinh Trung <trungbthuan@gmail.com>
2026-04-02 15:39:24 +0700

    Sửa phần test AI

--- app.js
+++ app.js
@@ -66,10 +66,12 @@ app.post("/analyze", async (req, res) => {
 
 app.get("/api/models", async (req, res) => {
     try {
-        const models = await genAI.listModels();
-        res.json(models);
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${process.env.GEMINI_API_KEY}`);
+        const data = await response.json();
+
+        res.send({ models: data.models });
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.send({ "Lỗi:": err });
     }
 });
 
